431dbfc8ea5e045fb6e29fff79c84c94ac67b772
Kevizy <44855147+Kevizy@users.noreply.github.com>
2020-01-23 11:50:11 -0800

    Add files via upload

---
+++ Min_Max_Values/src/min_max_values/Min_Max_Values.java
@@ -0,0 +1,53 @@
+
+package min_max_values;
+
+public class Min_Max_Values {
+
+    public static void main(String[] args) {
+        System.out.println("Minimum and maximum values"
+                + " of data in Java");
+        
+        System.out.println("\nByte Max: "
+        + Byte.MAX_VALUE); 
+        
+        System.out.println("Byte Min: " +
+                Byte.MIN_VALUE); 
+        
+        System.out.println("\nShort Max: " +
+                Short.MAX_VALUE); 
+        
+        System.out.println("Short Min: " +
+                Short.MIN_VALUE); 
+        
+        System.out.println("\nChar Max: " +
+                (Character.MAX_VALUE+0)); 
+        
+        System.out.println("Char Min: " +
+                (Character.MIN_VALUE+0));
+        
+        System.out.println("\nInt Max: " +
+                Integer.MAX_VALUE); 
+        
+        System.out.println("Int Min: " +
+                Integer.MIN_VALUE);
+        
+        System.out.println("\nLong Max: " +
+                Long.MAX_VALUE);
+        
+        System.out.println("Long Min: " +
+                Long.MIN_VALUE);
+        
+        System.out.println("\nFloat Max: " +
+                Float.MAX_VALUE);
+        
+        System.out.println("Float Min: " +
+                Float.MIN_VALUE);
+        
+        System.out.println("\nDouble Max: " + 
+                Double.MAX_VALUE);
+        
+        System.out.println("Double Min: " + 
+                Double.MIN_VALUE);
+    }
+    
+}
