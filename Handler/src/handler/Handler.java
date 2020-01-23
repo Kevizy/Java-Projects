dde34e73d628ac2fc7f9d78bf5d4f7166c9a26f6
Kevizy <44855147+Kevizy@users.noreply.github.com>
2020-01-23 11:52:34 -0800

    Add files via upload

---
+++ Handler/src/handler/Handler.java
@@ -0,0 +1,16 @@
+
+package handler;
+
+import javax.swing.JFrame; 
+public class Handler {
+
+    public static void main(String[] args) {
+        HandlerInfo msg = new HandlerInfo(); 
+        msg.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
+        msg.setSize(350, 100); 
+        msg.setVisible(true);
+        
+        
+    }
+    
+}
